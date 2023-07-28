a49feafd2810746174fc866267027c501b9d0df4
nbeverton <nbeverton@gmail.com>
2023-07-28 16:44:24 -0300

    Update ProdutoController.java
    Usando anotações para modificar valores persistidos. @PutMapping e @RequestMapping(method put e get)

--- src/main/java/projetospringboot/controllers/ProdutoController.java
+++ src/main/java/projetospringboot/controllers/ProdutoController.java
@@ -6,6 +6,7 @@ import projetospringboot.model.entities.Produto;
 import projetospringboot.model.repositories.ProdutoRepository;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/produtos")
@@ -14,9 +15,26 @@ public class ProdutoController{
     @Autowired
     private ProdutoRepository produtoRepository;
 
-    @PostMapping
-    public @ResponseBody Produto novoProduto(@Valid Produto produto) {
+//    @PostMapping
+    @RequestMapping(method = {RequestMethod.POST, RequestMethod.PUT})
+    public @ResponseBody Produto salvarProduto(@Valid Produto produto) {
         produtoRepository.save(produto);
         return produto;
     }
+
+    @GetMapping
+    public Iterable<Produto> obterProdutos(){
+        return produtoRepository.findAll();
+    }
+
+    @GetMapping(path = "/{id}")
+    public Optional<Produto> obterProdutoPorId(@PathVariable int id){
+        return produtoRepository.findById(id);
+    }
+
+//    @PutMapping
+//    public Produto alterarProduto(@Valid Produto produto){
+//        produtoRepository.save(produto);
+//        return produto;
+//    }
 }
